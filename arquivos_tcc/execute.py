59393ad9226401a6bf78bbd646332012a33bd970
Levi Tonon <114413223+levitonon@users.noreply.github.com>
2024-12-27 01:36:13 -0300

    Update execute.py
    Timer

--- arquivos_tcc/execute.py
+++ arquivos_tcc/execute.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 def executar_codigo1():
     print("Executando o script cdp_graph.py (Código 1)")
@@ -9,6 +10,11 @@ def executar_codigo2():
     subprocess.run(["python3", "map_per_locale.py"])  # Executa o código que irá criar o mapa
 
 if __name__ == "__main__":
-#Ordem de execução
+    inicio_total = time.time()  # Início do timer total
+    
+    # Ordem de execução
     executar_codigo1()  
-    executar_codigo2()  
+    executar_codigo2()
+    
+    fim_total = time.time()  # Fim do timer total
+    print(f"Tempo total de execução: {fim_total - inicio_total:.2f} segundos")
