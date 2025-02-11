2d466bd55d6e4579e8ca86756689341bbc621eba
levitonon <levitonon@hotmail.com>
2025-02-11 06:18:52 +0000

    Timer

--- arquivos_tcc/execute.py
+++ arquivos_tcc/execute.py
@@ -1,14 +1,25 @@
+import time
 import subprocess
 
 def executar_codigo1():
-    print("Executando o script cdp_graph.py (Código 1)")
-    subprocess.run(["python3", "cdp_graph.py"])  # Executa o código que irá varrer a rede
-
+    print("Executando o script cdp_graph.py")
+    subprocess.run(["python3", "cdp_graph.py"])
+    
 def executar_codigo2():
-    print("Executando o script create_map4.py (Código 2)")
-    subprocess.run(["python3", "map_per_locale.py"])  # Executa o código que irá criar o mapa
+    print("Executando o script create_map4.py")
+    subprocess.run(["python3", "map_per_locale.py"]) 
 
 if __name__ == "__main__":
-#Ordem de execução
-    executar_codigo1()  
-    executar_codigo2()  
+    #Timer do processo
+    start_time = time.time()
+
+    # Executa na ordem desejada
+    executar_codigo1()
+    executar_codigo2()
+
+    end_time = time.time()
+
+    total_time = end_time - start_time
+
+    # Exibe o tempo total formatado (2 casas decimais, por exemplo)
+    print(f"Tempo total de execução: {total_time:.2f} segundos")
